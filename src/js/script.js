66b01df485a1b474c73033ab63b01f91a4bddd3f
Piotr Stepien <naz@o2.pl>
2021-02-15 23:54:45 +0100

    Add working processOrder() from task 7.6

--- src/js/script.js
+++ src/js/script.js
@@ -65,7 +65,7 @@
       thisProduct.initOrderForm();
       thisProduct.processOrder();
 
-      console.log('new Product:',thisProduct);
+      //console.log('new Product:',thisProduct);
     }
 
     renderInMenu(){
@@ -123,7 +123,7 @@
 
     initOrderForm(){
       const thisProduct = this;
-      console.log('initOrderForm');
+      //console.log('initOrderForm');
 
       thisProduct.form.addEventListener('submit', function(event){
         event.preventDefault();
@@ -144,14 +144,44 @@
 
     processOrder(){
       const thisProduct = this;
-      console.log('processOrder');
+
+      // covert form to object structure e.g. { sauce: ['tomato'], toppings: ['olives', 'redPeppers']}
+      const formData = utils.serializeFormToObject(thisProduct.form);
+
+      // set price to default price
+      let price = thisProduct.data.price;
+
+      // for every category (param)...
+      for(let paramId in thisProduct.data.params) {
+        // determine param value, e.g. paramId = 'toppings', param = { label: 'Toppings', type: 'checkboxes'... }
+        const param = thisProduct.data.params[paramId];
+
+        // for every option in this category
+        for(let optionId in param.options) {
+          // determine option value, e.g. optionId = 'olives', option = { label: 'Olives', price: 2, default: true }
+          const option = param.options[optionId];
+
+          if(formData[paramId] && formData[paramId].includes(optionId)){
+            console.log('inside loop');
+            if(!option.defaul){
+              price += option.price;
+
+            } else if(option.default) {
+                price -= option.price;
+            }
+          }
+        }
+      }
+
+      // update calculated price in the HTML
+      thisProduct.priceElem.innerHTML = price;
     }
   }
 
   const app = {
     initMenu: function(){
       const thisApp = this;
-      console.log('thisApp.data:',thisApp.data);
+      //console.log('thisApp.data:',thisApp.data);
 
       for(let productData in thisApp.data.products){
         new Product(productData, thisApp.data.products[productData]);
