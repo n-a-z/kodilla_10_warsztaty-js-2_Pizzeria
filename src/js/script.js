cd7454b7413aa4ef54e7345723e16faf1763fcb4
Piotr Stepien <naz@o2.pl>
2021-02-20 23:42:35 +0100

    Fix missing semicolon

--- src/js/script.js
+++ src/js/script.js
@@ -345,6 +345,12 @@
 
       thisApp.data = dataSource;
     },
+    initCart: function(){
+      const thisApp = this;
+
+      const cartElem = document.querySelector(select.containerOf.cart);
+      thisApp.cart = new Cart(cartElem);
+    },
     init: function(){
       const thisApp = this;
       console.log('*** App starting ***');
@@ -355,6 +361,7 @@
 
       thisApp.initData();
       thisApp.initMenu();
+      thisApp.initCart();
     }
   };
 
