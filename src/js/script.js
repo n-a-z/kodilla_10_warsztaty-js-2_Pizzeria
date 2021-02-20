4df211c3d2286b709f08fa4a4d7d03b3bd88b33d
Piotr Stepien <naz@o2.pl>
2021-02-20 19:45:28 +0100

    Add template of class Cart

--- src/js/script.js
+++ src/js/script.js
@@ -311,6 +311,26 @@
     }
   }
 
+  class Cart{
+    constructor(element){
+      const thisCart = this;
+
+      thisCart.preventDefault = [];
+
+      thisCart.getElements(element);
+
+      console.log('new Cart', thisCart);
+    }
+
+    getElements(element){
+      const thisCart = this;
+
+      thisCart.dom = {};
+
+      thisCart.dom.wrapper = element;
+    }
+  }
+
   const app = {
     initMenu: function(){
       const thisApp = this;
