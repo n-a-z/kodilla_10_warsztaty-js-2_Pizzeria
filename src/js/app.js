a5f4b9b8cd7efcce866f61078ee5dfbcb668a313
Piotr Stepien <naz@o2.pl>
2021-03-23 17:28:46 +0100

    Add activatePage methos

--- src/js/app.js
+++ src/js/app.js
@@ -3,6 +3,15 @@ import Product from './components/Product.js';
 import Cart from './components/Cart.js';
 
 const app = {
+  initPages: function() {
+    const thisApp = this;
+
+    thisApp.pages = document.querySelector(select.containerOf.pages).children;
+    thisApp.navLinks = document.querySelectorAll(select.nav.links);
+
+    thisApp.activatePage(thisApp.pages[0].id);
+  },
+
   initMenu: function () {
     const thisApp = this;
     //console.log('thisApp.data:',thisApp.data);
@@ -15,6 +24,26 @@ const app = {
       );
     }
   },
+
+  activatePage: function(pageId) {
+    const thisApp = this;
+
+    /* add class "active" to matching pages, remove from non-matching */
+    for(let page of thisApp.pages) {
+      //console.log('page:',page);
+      //if(page.id === pageId) page.classNames.toggle(classNames.pages.active);
+      page.classList.toggle(classNames.pages.active, page.id === pageId);
+    }
+
+    /* add class "active" to matching links, remove from non-matching */
+    for(let link of thisApp.navLinks) {
+      link.classList.toggle(
+        classNames.nav.active,
+        link.getAttribute('href') === '#' + pageId
+      );
+    }
+  },
+
   initData: function () {
     const thisApp = this;
 
@@ -36,6 +65,7 @@ const app = {
       });
     console.log('thisApp.data:', JSON.stringify(thisApp.data));
   },
+
   initCart: function () {
     const thisApp = this;
 
@@ -56,6 +86,8 @@ const app = {
     console.log('classNames:', classNames);
     console.log('settings:', settings);
     console.log('templates:', templates);
+
+    thisApp.initPages();
 
     thisApp.initData();
     //thisApp.initMenu();
