68e3bff210da259a0ba93d4b61cf64b47e2ba0ad
Piotr Stepien <naz@o2.pl>
2021-03-24 08:51:52 +0100

    Add matching active pages based on their hash

--- src/js/app.js
+++ src/js/app.js
@@ -3,16 +3,30 @@ import Product from './components/Product.js';
 import Cart from './components/Cart.js';
 
 const app = {
-  initPages: function() {
+  initPages: function () {
     const thisApp = this;
 
     thisApp.pages = document.querySelector(select.containerOf.pages).children;
     thisApp.navLinks = document.querySelectorAll(select.nav.links);
 
-    thisApp.activatePage(thisApp.pages[0].id);
+    //thisApp.activatePage(thisApp.pages[0].id);
+    const idFromHash = window.location.hash.replace('#/', '');
+    //console.log(idFromHash);
 
-    for(let link of thisApp.navLinks){
-      link.addEventListener('click', function(event){
+    let pageMatchingHash = thisApp.pages[0].id;
+
+    for (let page of thisApp.pages) {
+      if (page.id === idFromHash) {
+        pageMatchingHash = page.id;
+        console.log(pageMatchingHash);
+        break;
+      }
+    }
+
+    thisApp.activatePage(pageMatchingHash);
+
+    for (let link of thisApp.navLinks) {
+      link.addEventListener('click', function (event) {
         const clickedElement = this;
         event.preventDefault();
 
@@ -21,6 +35,9 @@ const app = {
 
         /* run thisApp.activatePage with that id */
         thisApp.activatePage(id);
+
+        /* change URL hash */
+        window.location.hash = '#/' + id;
       });
     }
   },
@@ -38,18 +55,18 @@ const app = {
     }
   },
 
-  activatePage: function(pageId) {
+  activatePage: function (pageId) {
     const thisApp = this;
 
     /* add class "active" to matching pages, remove from non-matching */
-    for(let page of thisApp.pages) {
+    for (let page of thisApp.pages) {
       //console.log('page:',page);
       //if(page.id === pageId) page.classNames.toggle(classNames.pages.active);
       page.classList.toggle(classNames.pages.active, page.id === pageId);
     }
 
     /* add class "active" to matching links, remove from non-matching */
-    for(let link of thisApp.navLinks) {
+    for (let link of thisApp.navLinks) {
       link.classList.toggle(
         classNames.nav.active,
         link.getAttribute('href') === '#' + pageId
@@ -87,7 +104,7 @@ const app = {
 
     thisApp.productList = document.querySelector(select.containerOf.menu);
 
-    thisApp.productList.addEventListener('add-to-cart', function(event) {
+    thisApp.productList.addEventListener('add-to-cart', function (event) {
       app.cart.add(event.detail.product);
     });
   },
