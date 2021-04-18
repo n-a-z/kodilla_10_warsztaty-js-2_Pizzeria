c3b595e14bedfa1fcd4a037b6066851926f14af4
naz <naz@o2.pl>
2021-04-18 19:10:03 +0200

    Add changes required for publication on a server

--- src/js/settings.js
+++ src/js/settings.js
@@ -102,7 +102,10 @@ export const classNames = {
 
 export const settings = {
   db: {
-    url: '//localhost:3131',
+    url:
+      '//' +
+      window.location.hostname +
+      (window.location.hostname == 'localhost' ? ':3131' : ''),
     product: 'product',
     order: 'order',
     booking: 'booking',
